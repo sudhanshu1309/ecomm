67afca73f08537095c7a81486c1579aef81b5b2d
sudhanshu1309 <sudhanshutripathi008@gmail.com>
2021-11-11 01:26:04 +0530

    commit on 11 Nov 2021

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import API from "../backend";
 import "../styles.css";
 import Base from "./Base";
+import Card from "./Card";
+import { getProducts } from "./helper/coreapicalls";
 
 const Home = () => {
-  console.log("API IS", API);
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadAllProducts = () => {
+    getProducts().then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProducts(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
   return (
     <Base title="Home Page" description="Welcome to the t-shirt store">
-      <div className="row">
-        <div className="col-4">
-          <button className="btn btn-success">Test</button>
-        </div>
-        <div className="col-4">
-          <button className="btn btn-success">Test</button>
-        </div>
-        <div className="col-4">
-          <button className="btn btn-success">Test</button>
+      <div className="row text-center">
+        <h1 className="text-white">All of tshirts</h1>
+        <div className="row">
+          {products.map((product, index) => {
+            return (
+              <div key={index} className="col-2 mb-4">
+                <Card />
+              </div>
+            );
+          })}
         </div>
       </div>
     </Base>
